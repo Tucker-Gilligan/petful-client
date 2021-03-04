e603d29245c48a0cde6fee8169a98c3191ad7f6d
Tucker <gillitux@gmail.com>
2021-03-04 14:13:35 -0500

    changed up landing page a little bit

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -7,10 +7,13 @@ function LandingPage() {
       <p>Welcome to FIFO, where we make the adoption process easy</p>
       <p>
         When you choose to start the adoption process, you will be placed in a
-        queue
+        queue.
       </p>
-      <p>While you wait, you can see which pets are currently being rescued!</p>
-      <p>Don't worry, you'll always be able to see your place in line:) </p>
+      <p>While you wait, you can see which pets are currently being rescued.</p>
+      <p>
+        You'll always be able to see your place in line - you'll never have to
+        wait long.
+      </p>
       <p>Start the adoption process now, and leave with a new furry friend:)</p>
       <Link to="/adopt">
         <button className="petful__button">Start</button>
